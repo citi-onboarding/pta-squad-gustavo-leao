3e1979d149886f7e2193c21a5125a36e0db8ec92
dantteroberto-draaf <draaf@cin.ufpe.br>
2026-05-21 16:20:10 -0300

    feat/componenteLivro: adding buttons and adjusting stylization

--- client/src/components/livro/index.tsx
+++ client/src/components/livro/index.tsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 
-export function CardLivro() {
+import { Bookmark, Eye, Trash2 } from "lucide-react";
+
+import { Button } from "../ui/button";
+
+interface CardLivroProps {
+    title?: string;
+    autor?: string;
+    category?: string;
+    totalQty?: number;
+}
+
+export function CardLivro({ title = "Clean Code", autor = "Robert C. Martin", category = "Tecnologia", totalQty = 5 }: CardLivroProps) {   // default values
     const [isOpenEmprestimo, setIsOpenEmprestimo] = useState(false);
     const [isOpenDetalhe, setIsOpenDetalhes] = useState(false);
 
-    const bookInfo = { title: "Clean Code", autor: "Robert C. Martin", category: "Tecnologia", totalQty: 5 }; // mocked data
 
     const onLoan = () => { setIsOpenEmprestimo(true) }
 
@@ -14,16 +24,30 @@ export function CardLivro() {
 
     return(
         <div>
-            <div className="w-96 h-auto flex-col rounded-lg shadow-lg"> {/* CardLivro */}
+            <div className="w-80 h-auto flex-col rounded-lg shadow-lg"> {/* CardLivro */}
                 <div className="w-full h-48 bg-gray-100 rounded-t-lg">
                     {/* Image here */}
                 </div>
 
                 <div className="flex-col gap-2 px-4 py-2">
-                    <p className="text-lg text-black font-medium">{bookInfo.title}</p>
-                    <p className=" text-gray-700">{bookInfo.autor}</p>
-                    <p className="text-sm text-green-400">{bookInfo.category}</p>
-                    <p className="text-sm">Disponível: {bookInfo.totalQty} unidade(s)</p>
+                    <p className="text-lg text-black font-medium">{title}</p>
+                    <p className=" text-gray-700">{autor}</p>
+                    <p className="text-sm text-green-400">{category}</p>
+                    <p className="text-sm">Disponível: {totalQty} unidade(s)</p>
+                </div>
+
+                <div className="w-full flex px-4 pb-3 pt-3 gap-2">
+                    <Button onClick={onDetails} className="flex-2 text-green-400 border-green-400 border-2 text-md" variant={"outline"}>
+                        <Eye></Eye>Ver
+                    </Button>
+
+                    <Button onClick={onLoan} className="flex-1 bg-green-400 text-md">
+                        <Bookmark></Bookmark>Emprestar
+                    </Button>
+
+                    <Button onClick={onDelete} variant={"destructive"}>
+                        <Trash2></Trash2>
+                    </Button>
                 </div>
 
             </div>
