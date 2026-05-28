cc58de2ec926361d62889035c89d465847cada84
Victor <vap4@cin.ufpe.br>
2026-05-28 00:54:57 -0300

    feat: enable CORS for frontend integration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,17 @@
 import routes from "./routes";
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import "@database";
 
 dotenv.config();
-
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use(routes);
 app.use(express.static(__dirname + "/public"));
